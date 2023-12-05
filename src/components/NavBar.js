db3e02922212e46e5ef67f19f0c8315ecde2594c
Mushbt <mustafa_habet@hotmail.com>
2023-12-05 12:17:30 +0000

    Add further styling to navbar, add Routing and connect to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,31 +2,34 @@ import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import logo from '../assets/tv_logo.png'
 import styles from '../styles/NavBar.module.css'
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
     return (
       <Navbar className={styles.NavBar} expand="md" fixed="top">
         <Container>
-          <Navbar.Brand>
+          <NavLink to="/">
+            <Navbar.Brand>
               <img src={logo} alt="TravelerViews logo" height="200" width="200" />
-          </Navbar.Brand>
+            </Navbar.Brand>
+          </NavLink>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto text-right">
-            <Nav.Link>
-                <i class="fa-solid fa-house"></i>Home
-            </Nav.Link>
-            <Nav.Link>
-                <i class="fa-solid fa-user-plus"></i>Sign up
-            </Nav.Link>
-            <Nav.Link>
-                <i class="fa-solid fa-right-to-bracket"></i>Log in
-            </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
-  };
+            <Nav className="ml-auto text-right">
+              <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">
+                <i className="fa-solid fa-house"></i>Home
+              </NavLink>
+            < NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">
+              <i className="fa-solid fa-user-plus"></i>Sign up
+            </NavLink>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/login">
+              <i className="fa-solid fa-right-to-bracket"></i>Log in
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
   
-  export default NavBar;
+export default NavBar;
