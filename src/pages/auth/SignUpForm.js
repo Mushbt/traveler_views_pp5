302b5909d9cdfd97e4d0b79648062ca90d2672be
Mushbt <mustafa_habet@hotmail.com>
2023-12-05 14:37:22 +0000

    Add initial styling to sign up form

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,27 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
 import styles from "../../styles/SignUpForm.module.css";
-import { Col, Row, Container } from "react-bootstrap";
+import { Form, Button, Col, Row, Container } from "react-bootstrap";
+import appStyles from "../../App.module.css";
 
 const SignUpForm = () => {
-    return (
-      <Row className="text-center">
-      <Col className="my-auto py-2 p-md-2" >
-          <Container>
-            <h1>Sign up</h1>
-  
-              {/* sign up form will go here */}
-  
-          </Container>
-          <Container className="mt-3">
-            <Link to="/login">
-              Already a member? Click <span>here</span> to log in.
-            </Link>
-          </Container>
+  return (
+    <Row>
+        <Col className="my-auto offset-md-2" md={8}>
+            <Container className={`${appStyles.Content} p-4 `}>
+                <h1>Sign up</h1>
+                    <Form>
+                        <Form.Group controlId="username">
+                            <Form.Label className="d-none">Username</Form.Label>
+                            <Form.Control className={styles.Input} type="text" placeholder="Username" name="username" />
+                        </Form.Group>
+                        <Form.Group controlId="password1">
+                            <Form.Label className="d-none">Password</Form.Label>
+                            <Form.Control className={styles.Input} type="password" placeholder="Password" name="password1"/>
+                        </Form.Group>
+                        <Form.Group controlId="password2">
+                            <Form.Label className="d-none">Confirm password</Form.Label>
+                            <Form.Control className={styles.Input} type="password" placeholder="Confirm password" name="password2"/>
+                        </Form.Group>
+                        <button type="submit">
+                            Sign Up!
+                        </button>
+                    </Form>
+            </Container>
+            <Container className={`mt-3 ${appStyles.Content}`}>
+                <Link className={styles.Link} to="/login">
+                    Already a member? Click <span>here</span> to log in.
+                </Link>
+            </Container>
         </Col>
-      </Row>
-    );
-  };
-  
-  export default SignUpForm;
+    </Row>
+  );
+};
+
+export default SignUpForm;
