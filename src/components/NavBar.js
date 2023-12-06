8f21926dd631c0e7421c63f881cae860888cd87a
Mushbt <mustafa_habet@hotmail.com>
2023-12-06 09:44:38 +0000

    Add useContext hook for condtional rendering in navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,35 +1,55 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import logo from '../assets/tv_logo.png'
 import styles from '../styles/NavBar.module.css'
 import { NavLink } from "react-router-dom";
+import { CurrentUserContext } from '../App';
 
 const NavBar = () => {
-    return (
-      <Navbar className={styles.NavBar} expand="md" fixed="top">
-        <Container>
-          <NavLink to="/">
-            <Navbar.Brand>
-              <img src={logo} alt="TravelerViews logo" height="200" width="200" />
-            </Navbar.Brand>
-          </NavLink>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto text-right">
-              <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">
-                <i className="fa-solid fa-house"></i>Home
-              </NavLink>
-            < NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">
-              <i className="fa-solid fa-user-plus"></i>Sign up
+  const currentUser = useContext(CurrentUserContext);
+
+  // Variable to display current username in the navbar
+  const loggedInNavBar = <>{currentUser?.username}</>
+
+  // Navbar visible to users not logged-in
+  const loggedOutNavBar = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signup"
+      >
+        <i className="fa-solid fa-user-plus"></i>Sign up
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/login"
+      >
+        <i className="fa-solid fa-right-to-bracket"></i>Log in
+      </NavLink>
+    </>
+  );
+  return (
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Container>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="TravelerViews logo" height="200" width="200" />
+          </Navbar.Brand>
+        </NavLink>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-right">
+            <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">
+              <i className="fa-solid fa-house"></i>Home
             </NavLink>
-            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/login">
-              <i className="fa-solid fa-right-to-bracket"></i>Log in
-            </NavLink>
+            {currentUser ? loggedInNavBar : loggedOutNavBar}
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 };
-  
+
 export default NavBar;
