9542e4716e5934eb80255af1aef20275c56bbb8b
Mushbt <mustafa_habet@hotmail.com>
2023-12-07 13:46:49 +0000

    Add API request to PostPage to fetch post by ID

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,32 +1,47 @@
-import React from "react";
-
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
-
+import React, { useEffect, useState } from "react";
+import { Row, Col, Container } from "react-bootstrap";
 import appStyles from "../../App.module.css";
+import { useParams } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function PostPage() {
-  // Add your logic here
+    const { id } = useParams();
+    const [post, setPost] = useState({ results: [] });
 
-  return (
-    <Container>
-      <Row>
-        <Col className="py-2 p-0 p-lg-2" lg={4}>
-          <Container className={`${appStyles.Content} ${appStyles.CollapsedColumn} mb-3`}>Like, feed, add</Container>
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                const [{ data: post }] = await Promise.all([
+                    axiosReq.get(`/posts/${id}`),
+                ]);
+                setPost({ results: [post] });
+                console.log(post);
+            } catch (err) {
+                console.log(err);
+            }
+        };
 
-          <Container className={`${appStyles.Content} ${appStyles.CollapsedColumn} mb-3`}>Popular users</Container>
+        handleMount();
+    }, [id]);
 
-          <Container className={`${appStyles.Content} ${appStyles.CollapsedColumn} mb-3`}>Country</Container>
-        </Col>
+    return (
+        <Container>
+            <Row>
+                <Col className="py-2 p-0 p-lg-2" lg={4}>
+                    <Container className={`${appStyles.Content} ${appStyles.CollapsedColumn} mb-3`}>Like, feed, add</Container>
 
-        <Col className="py-2 p-0 p-lg-2" lg={8}>
-          <p>Post component</p>
-          <Container className={appStyles.Content}>Comments</Container>
-        </Col>
-      </Row>
-    </Container>
-  );
+                    <Container className={`${appStyles.Content} ${appStyles.CollapsedColumn} mb-3`}>Popular users</Container>
+
+                    <Container className={`${appStyles.Content} ${appStyles.CollapsedColumn} mb-3`}>Country</Container>
+                </Col>
+
+                <Col className="py-2 p-0 p-lg-2" lg={8}>
+                    <p>Post component</p>
+                    <Container className={appStyles.Content}>Comments</Container>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default PostPage;
