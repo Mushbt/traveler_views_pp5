9c96939009f644b467be1de0c7b71d3608fc031f
Mushbt <mustafa_habet@hotmail.com>
2023-12-07 18:37:58 +0000

    Add search bar for posts and add styling

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -13,20 +13,23 @@ function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
+
+  const [query, setQuery] = useState("");
+
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const { data } = await axiosReq.get(`/posts/?${filter}`);
+        const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
         setPosts(data);
         setHasLoaded(true);
       } catch (err) {
-        // console.log(err);
+        console.log(err);
       }
     };
 
     setHasLoaded(false);
     fetchPosts();
-  }, [filter, pathname]);
+  }, [filter, query, pathname]);
 
   return (
     <Container>
@@ -49,6 +52,20 @@ function PostsPage({ message, filter = "" }) {
         </Col>
 
         <Col className="py-2 p-0 p-lg-2" lg={8}>
+
+          <i className={`fas fa-search ${styles.SearchIcon}`} />
+          <Form
+            className={styles.SearchBar}
+            onSubmit={(e) => e.preventDefault()}
+          >
+            <Form.Control
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              type="text"
+              className="mr-sm-2"
+              placeholder="Search posts"
+            />
+          </Form>
 
           {hasLoaded ? (
             <>
