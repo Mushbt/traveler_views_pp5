4acfad9c3e08140ce2d44f9ec28cfb87047d292b
Mushbt <mustafa_habet@hotmail.com>
2023-12-07 14:22:59 +0000

    Add PostPage with post details and add initial styling

---
+++ src/pages/posts/Post.js
@@ -0,0 +1,83 @@
+import React from "react";
+import styles from "../../styles/Post.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Card, Media, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Avatar from "../../components/Avatar";
+
+const Post = (props) => {
+    const {
+        id,
+        owner,
+        profile_id,
+        profile_image,
+        title,
+        description,
+        country,
+        comments_number,
+        likes_number,
+        like_id,
+        image,
+        updated_on,
+        postPage,
+    } = props;
+
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
+
+    return (
+        <Card className={styles.Post}>
+            <Card.Body>
+                <Media className="align-items-center justify-content-between">
+                    <Link to={`/profiles/${profile_id}`}>
+                        <Avatar src={profile_image} height={50} />
+                        {owner}
+                    </Link>
+                    <div className="d-flex align-items-center">
+                        <span>{updated_on}</span>
+                        {is_owner && postPage && "..."}
+                    </div>
+                </Media>
+            </Card.Body>
+            <Link to={`/posts/${id}`}>
+                <Card.Img src={image} alt={title} />
+            </Link>
+            <Card.Body>
+                {title && <Card.Title className="text-center">{title}</Card.Title>}
+                {description && <Card.Text>{description}</Card.Text>}
+                {country && <Card.Text>{country}</Card.Text>}
+                <hr className={styles.Line} />
+                <div>
+                    {is_owner ? (
+                        <OverlayTrigger
+                            placement="top"
+                            overlay={<Tooltip>You can't like your own post!</Tooltip>}
+                        >
+                            <i className="far fa-heart" />
+                        </OverlayTrigger>
+                    ) : like_id ? (
+                        <span onClick={() => { }}>
+                            <i className="fas fa-heart" />
+                        </span>
+                    ) : currentUser ? (
+                        <span onClick={() => { }}>
+                            <i className="far fa-heart" />
+                        </span>
+                    ) : (
+                        <OverlayTrigger>
+                            <i className="far fa-heart" />
+                        </OverlayTrigger>
+                    )}
+
+                    {likes_number}
+                    <Link to={`/posts/${id}`}>
+                        <i className="far fa-comments" />
+                    </Link>
+                    {comments_number}
+                </div>
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default Post;
