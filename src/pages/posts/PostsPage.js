a195e32b6c278de4fe38baa65d4858e2504ab64b
Mushbt <mustafa_habet@hotmail.com>
2023-12-07 18:09:11 +0000

    Add function to display posts on PostsPage

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Container, Form, Row } from "react-bootstrap";
 import appStyles from "../../App.module.css";
 import styles from "../../styles/PostsPage.module.css";
+import Post from "./Post";
+import Asset from "../../components/Asset";
 import SecondaryNavBar from "../../components/SecondaryNavBar";
+import { useLocation } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
 
+function PostsPage({ message, filter = "" }) {
+  const [posts, setPosts] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const { data } = await axiosReq.get(`/posts/?${filter}`);
+        setPosts(data);
+        setHasLoaded(true);
+      } catch (err) {
+        // console.log(err);
+      }
+    };
 
-function PostsPage() {
+    setHasLoaded(false);
+    fetchPosts();
+  }, [filter, pathname]);
+
   return (
     <Container>
       <Row>
@@ -27,6 +48,25 @@ function PostsPage() {
         </Col>
 
         <Col className="py-2 p-0 p-lg-2" lg={8}>
+
+          {hasLoaded ? (
+            <>
+              {posts.results.length ? (
+                posts.results.map((post) => (
+                  <Post key={post.id} {...post} setPosts={setPosts} />
+                ))
+              ) : (
+                <Container className={appStyles.Content}>
+                  <Asset src={NoResultsImage} width={20} height={20} message={message} />
+                </Container>
+              )}
+            </>
+          ) : (
+            <Container className={appStyles.Content}>
+              <Asset spinner />
+            </Container>
+          )}
+
         </Col>
       </Row>
     </Container>
