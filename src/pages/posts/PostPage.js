5a41dc49634c192db7cd75582b550ab7977ef861
Mushbt <mustafa_habet@hotmail.com>
2023-12-08 20:46:58 +0000

    Add Comment component and add styling

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -6,6 +6,7 @@ import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Comment from "../comments/Comment"
 
 function PostPage() {
     const { id } = useParams();
@@ -18,11 +19,12 @@ function PostPage() {
     useEffect(() => {
         const handleMount = async () => {
             try {
-                const [{ data: post }] = await Promise.all([
+                const [{ data: post }, { data: comments }] = await Promise.all([
                     axiosReq.get(`/posts/${id}`),
+                    axiosReq.get(`/comments/?post=${id}`)
                 ]);
                 setPost({ results: [post] });
-                console.log(post);
+                console.log(comments);
             } catch (err) {
                 console.log(err);
             }
@@ -56,6 +58,15 @@ function PostPage() {
                         ) : comments.results.length ? (
                             "Comments"
                         ) : null}
+                        {comments.results.length ? (
+                            comments.results.map((comment) => (
+                                <Comment key={comment.id} {...comment} />
+                            ))
+                        ) : currentUser ? (
+                            <span>Be the first one to comment!</span>
+                        ) : (
+                            <span>No comments</span>
+                        )}
                     </Container>
                 </Col>
             </Row>
