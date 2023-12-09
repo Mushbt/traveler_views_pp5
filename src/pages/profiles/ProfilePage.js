c9ae5b34744fd96c54c9d7b0302d89b5123416ac
Mushbt <mustafa_habet@hotmail.com>
2023-12-09 13:11:52 +0000

    Add API to request ProfileData

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -4,71 +4,92 @@ import Asset from "../../components/Asset";
 import appStyles from "../../App.module.css";
 import PopularProfiles from "./PopularProfiles";
 import SecondaryNavBar from "../../components/SecondaryNavBar";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { useParams } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useSetProfileData } from "../../contexts/ProfileDataContext";
 
 function ProfilePage() {
     const [hasLoaded, setHasLoaded] = useState(false);
-  
+    const currentUser = useCurrentUser();
+    const { id } = useParams();
+    const setProfileData = useSetProfileData();
+
     useEffect(() => {
-        setHasLoaded(true);
-    }, [])
-  
+        const fetchData = async () => {
+            try {
+                const [{ data: pageProfile }] = await Promise.all([
+                    axiosReq.get(`/profiles/${id}/`),
+                ]);
+                setProfileData((prevState) => ({
+                    ...prevState,
+                    pageProfile: { results: [pageProfile] },
+                }));
+                setHasLoaded(true);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        fetchData();
+    }, [id, setProfileData]);
+
     const mainProfile = (
-      <>
-        <Row noGutters className="px-3 text-center">
-          <Col lg={3} className="text-lg-left">
-            <p>Image</p>
-          </Col>
-          <Col lg={6}>
-            <h3 className="m-2">Username</h3>
-            <p>Number of followers, people following & posts</p>
-          </Col>
-          <Col lg={3} className="text-lg-right">
-          <p>Follow button</p>
-          </Col>
-          <Col className="p-3">Profile description</Col>
-        </Row>
-      </>
+        <>
+            <Row noGutters className="px-3 text-center">
+                <Col lg={3} className="text-lg-left">
+                    <p>Image</p>
+                </Col>
+                <Col lg={6}>
+                    <h3 className="m-2">Username</h3>
+                    <p>Number of followers, people following & posts</p>
+                </Col>
+                <Col lg={3} className="text-lg-right">
+                    <p>Follow button</p>
+                </Col>
+                <Col className="p-3">Profile description</Col>
+            </Row>
+        </>
     );
-  
+
     const mainProfilePosts = (
-      <>
-        <hr />
-        <p className="text-center">Profile owner's posts</p>
-        <hr />
-      </>
+        <>
+            <hr />
+            <p className="text-center">Profile owner's posts</p>
+            <hr />
+        </>
     );
-  
+
     return (
-      <Container>
-        <Row>
-          <Col className="pt-2 p-0 g-0" lg={4}>
-              <SecondaryNavBar />
-  
-            <Container
-              className={`${appStyles.Content}mb-2`}
-            >
-              <PopularProfiles />
-            </Container>
-          </Col>
-  
-          <Col className="py-2 p-0 p-lg-2" lg={8}>
-  
-      <Container className={appStyles.Content}>
-          {hasLoaded ? (
-              <>
-                {mainProfile}
-                {mainProfilePosts}
-              </>
-            ) : (
-              <Asset spinner />
-            )}
-          </Container>
-  
-          </Col>
-        </Row>
-      </Container>
-  
+        <Container>
+            <Row>
+                <Col className="pt-2 p-0 g-0" lg={4}>
+                    <SecondaryNavBar />
+
+                    <Container
+                        className={`${appStyles.Content}mb-2`}
+                    >
+                        <PopularProfiles />
+                    </Container>
+                </Col>
+
+                <Col className="py-2 p-0 p-lg-2" lg={8}>
+
+                    <Container className={appStyles.Content}>
+                        {hasLoaded ? (
+                            <>
+                                {mainProfile}
+                                {mainProfilePosts}
+                            </>
+                        ) : (
+                            <Asset spinner />
+                        )}
+                    </Container>
+
+                </Col>
+            </Row>
+        </Container>
+
     );
-  }
-  
-  export default ProfilePage;
+}
+
+export default ProfilePage;
