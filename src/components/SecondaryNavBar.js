9e510d2537d3255bf097e2610f44aa0ec5cc419d
Mushbt <mustafa_habet@hotmail.com>
2023-12-10 15:29:52 +0000

    Refactor SecondaryNavBar as logged in users were not able to press on icons

--- src/components/SecondaryNavBar.js
+++ src/components/SecondaryNavBar.js
@@ -1,36 +1,66 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
+import { Container, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import appStyles from '../App.module.css';
 import styles from '../styles/SecondaryNavBar.module.css';
+import { useCurrentUser } from '../contexts/CurrentUserContext';
 
-const SecondaryNavBar = ({ userAuthStatus }) => {
-  const handleLogout = () => {};
-  const handleLinkClick = (event) => {
-    if (!userAuthStatus) {
-      event.preventDefault();
-    }
-  };
+const SecondaryNavBar = () => {
+  const currentUser = useCurrentUser();
+
+  const renderTooltip = (message) => (
+    <Tooltip id="tooltip">{message}</Tooltip>
+  );
 
   return (
     <Container
       className={`${appStyles.Content} ${appStyles.CollapsedColumn} mb-3 ${styles.FlexDisplay}`}
     >
-      <Link to="/posts/create" onClick={handleLinkClick}>
-        <i className="fa-regular fa-plus fa-fw"></i>
-        <p className={styles.Link}>Add post</p>
-      </Link>
-      <Link to="/liked" onClick={handleLinkClick}>
-        <i className="fa-regular fa-heart fa-fw"></i>
-        <p className={styles.Link}>Liked posts</p>
-      </Link>
-      <Link to="/feed" onClick={handleLinkClick}>
-        <i className="fa-solid fa-rss fa-fw"></i>
-        <p className={styles.Link}>Feed</p>
-      </Link>
-
-      {userAuthStatus && (
-        <button onClick={handleLogout}>Logout</button>
+      {currentUser ? (
+        <>
+          <Link to="/posts/create">
+            <i className="fa-regular fa-plus fa-fw"></i>
+            <p className={styles.Link}>Add post</p>
+          </Link>
+          <Link to="/liked">
+            <i className="fa-regular fa-heart fa-fw"></i>
+            <p className={styles.Link}>Liked posts</p>
+          </Link>
+          <Link to="/feed">
+            <i className="fa-solid fa-rss fa-fw"></i>
+            <p className={styles.Link}>Feed</p>
+          </Link>
+        </>
+      ) : (
+        <>
+          <OverlayTrigger
+            placement="bottom"
+            overlay={renderTooltip('Log in to create a post')}
+          >
+            <div>
+              <i className="fa-regular fa-plus fa-fw"></i>
+              <p className={styles.Link}>Add post</p>
+            </div>
+          </OverlayTrigger>
+          <OverlayTrigger
+            placement="bottom"
+            overlay={renderTooltip('Log in to access liked photos')}
+          >
+            <div>
+              <i className="fa-regular fa-heart fa-fw"></i>
+              <p className={styles.Link}>Liked posts</p>
+            </div>
+          </OverlayTrigger>
+          <OverlayTrigger
+            placement="bottom"
+            overlay={renderTooltip('Log in to access the feed')}
+          >
+            <div>
+              <i className="fa-solid fa-rss fa-fw"></i>
+              <p className={styles.Link}>Feed</p>
+            </div>
+          </OverlayTrigger>
+        </>
       )}
     </Container>
   );
