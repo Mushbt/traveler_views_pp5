83f654ce9a312ed6f4565c57717d2688ee550095
Mushbt <mustafa_habet@hotmail.com>
2023-12-10 14:45:41 +0000

    Add user authentication to SecondaryNavBar so users who are not logged in cannot click on any of the icons

--- src/components/SecondaryNavBar.js
+++ src/components/SecondaryNavBar.js
@@ -1,19 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Container } from "react-bootstrap";
-import appStyles from "../App.module.css";
-import styles from "../styles/SecondaryNavBar.module.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import appStyles from '../App.module.css';
+import styles from '../styles/SecondaryNavBar.module.css';
 
-const SecondaryNavBar = () => {
-    return (
-        <Container
-            className={`${appStyles.Content} ${appStyles.CollapsedColumn} mb-3 ${styles.FlexDisplay}`}
-        >
-            <Link to="/posts/create"><i class="fa-regular fa-plus fa-fw"></i><p className={styles.Link}>Add post</p></Link>
-            <Link to="/liked"><i class="fa-regular fa-heart fa-fw"></i><p className={styles.Link}>Liked posts</p></Link>
-            <Link to="/feed"><i class="fa-solid fa-rss fa-fw"></i><p className={styles.Link}>Feed</p></Link>
-        </Container>
-    );
+const SecondaryNavBar = ({ userAuthStatus }) => {
+  const handleLogout = () => {};
+  const handleLinkClick = (event) => {
+    if (!userAuthStatus) {
+      event.preventDefault();
+    }
+  };
+
+  return (
+    <Container
+      className={`${appStyles.Content} ${appStyles.CollapsedColumn} mb-3 ${styles.FlexDisplay}`}
+    >
+      <Link to="/posts/create" onClick={handleLinkClick}>
+        <i className="fa-regular fa-plus fa-fw"></i>
+        <p className={styles.Link}>Add post</p>
+      </Link>
+      <Link to="/liked" onClick={handleLinkClick}>
+        <i className="fa-regular fa-heart fa-fw"></i>
+        <p className={styles.Link}>Liked posts</p>
+      </Link>
+      <Link to="/feed" onClick={handleLinkClick}>
+        <i className="fa-solid fa-rss fa-fw"></i>
+        <p className={styles.Link}>Feed</p>
+      </Link>
+
+      {userAuthStatus && (
+        <button onClick={handleLogout}>Logout</button>
+      )}
+    </Container>
+  );
 };
 
 export default SecondaryNavBar;
