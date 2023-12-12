3e5465f4c0249d68a676d8e591413cffe30d2718
Mushbt <mustafa_habet@hotmail.com>
2023-12-12 10:32:16 +0000

    Add Alert for when a post is deleted and adjust redirect to go to home page

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,135 +1,162 @@
 import React from "react";
 import styles from "../../styles/Post.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
-import { Card, Media, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { Card, Media, OverlayTrigger, Tooltip, Alert } from "react-bootstrap";
 import { Link, useHistory } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import { axiosRes } from "../../api/axiosDefaults";
 import { DropdownMenu } from "../../components/DropdownMenu";
+import { useState } from "react";
 
 const Post = (props) => {
-    const {
-        id,
-        owner,
-        profile_id,
-        profile_image,
-        title,
-        description,
-        country,
-        comments_number,
-        likes_number,
-        like_id,
-        image,
-        updated_on,
-        postPage,
-        setPosts,
-    } = props;
+  const {
+    id,
+    owner,
+    profile_id,
+    profile_image,
+    title,
+    description,
+    country,
+    comments_number,
+    likes_number,
+    like_id,
+    image,
+    updated_on,
+    postPage,
+    setPosts,
+  } = props;
 
-    const currentUser = useCurrentUser();
-    const is_owner = currentUser?.username === owner;
-    const history = useHistory();
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+  const history = useHistory();
+  const [showSuccessAlert, setShowSuccessAlert] = useState(false);
+  const [showErrorAlert, setShowErrorAlert] = useState(false);
 
-    const handleEdit = () => {
-        history.push(`/posts/${id}/edit`);
-    };
+  const handleEdit = () => {
+    history.push(`/posts/${id}/edit`);
+  };
 
-    const handleDelete = async () => {
-        try {
-            await axiosRes.delete(`/posts/${id}/`);
-            history.goBack();
-        } catch (err) {
-            console.log(err);
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/posts/${id}/`);
+      setPosts((prevPosts) => ({
+        ...prevPosts,
+        results: prevPosts.results.filter((post) => post.id !== id),
+      }));
+      setShowSuccessAlert(true);
+      setTimeout(() => {
+        setShowSuccessAlert(false);
+        if (postPage) {
+          history.push("/");
         }
-    };
+      }, 5000);
+    } catch (err) {
+      console.log(err);
+      setShowErrorAlert(true);
+      setTimeout(() => {
+        setShowErrorAlert(false);
+      }, 5000);
+    }
+  };
 
-    const handleLike = async () => {
-        try {
-            const { data } = await axiosRes.post("/likes/", { post: id });
-            setPosts((prevPosts) => ({
-                ...prevPosts,
-                results: prevPosts.results.map((post) => {
-                    return post.id === id
-                        ? { ...post, likes_number: post.likes_number + 1, like_id: data.id }
-                        : post;
-                }),
-            }));
-        } catch (err) {
-            console.log(err);
-        }
-    };
+  const handleLike = async () => {
+    try {
+      const { data } = await axiosRes.post("/likes/", { post: id });
+      setPosts((prevPosts) => ({
+        ...prevPosts,
+        results: prevPosts.results.map((post) => {
+          return post.id === id
+            ? { ...post, likes_number: post.likes_number + 1, like_id: data.id }
+            : post;
+        }),
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-    const handleUnlike = async () => {
-        try {
-            await axiosRes.delete(`/likes/${like_id}/`);
-            setPosts((prevPosts) => ({
-                ...prevPosts,
-                results: prevPosts.results.map((post) => {
-                    return post.id === id
-                        ? { ...post, likes_number: post.likes_number - 1, like_id: null }
-                        : post;
-                }),
-            }));
-        } catch (err) {
-            console.log(err);
-        }
-    };
+  const handleUnlike = async () => {
+    try {
+      await axiosRes.delete(`/likes/${like_id}/`);
+      setPosts((prevPosts) => ({
+        ...prevPosts,
+        results: prevPosts.results.map((post) => {
+          return post.id === id
+            ? { ...post, likes_number: post.likes_number - 1, like_id: null }
+            : post;
+        }),
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-    return (
-        <Card className={styles.Post}>
-            <Card.Body>
-                <Media className="align-items-center justify-content-between">
-                    <Link to={`/profiles/${profile_id}`}>
-                        <Avatar src={profile_image} height={50} />
-                        {owner}
-                    </Link>
-                    <div className="d-flex align-items-center">
-                        <span>{updated_on}</span>
-                        {is_owner && postPage && <DropdownMenu handleEdit={handleEdit} handleDelete={handleDelete} />}
-                    </div>
-                </Media>
-            </Card.Body>
-            <Link to={`/posts/${id}`}>
-                <Card.Img src={image} alt={title} />
-            </Link>
-            <Card.Body>
-                {title && <Card.Title className="text-center">{title}</Card.Title>}
-                {description && <Card.Text>{description}</Card.Text>}
-                {country && <Card.Text>{country}</Card.Text>}
-                <hr className={styles.Line} />
-                <div className={styles.PostBar}>
-                    {is_owner ? (
-                        <OverlayTrigger
-                            placement="top"
-                            overlay={<Tooltip>You can't like your own post!</Tooltip>}
-                        >
-                            <i className="far fa-heart" />
-                        </OverlayTrigger>
-                    ) : like_id ? (
-                        <span onClick={handleUnlike}>
-                            <i className="fas fa-heart" />
-                        </span>
-                    ) : currentUser ? (
-                        <span onClick={handleLike}>
-                            <i className="far fa-heart" />
-                        </span>
-                    ) : (
-                        <OverlayTrigger
-                            placement="top"
-                            overlay={<Tooltip>Log in to like posts!</Tooltip>}
-                        >
-                            <i className="far fa-heart" />
-                        </OverlayTrigger>
-                    )}
+  return (
+    <Card className={styles.Post}>
+      <Card.Body>
+        <Media className="align-items-center justify-content-between">
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profile_image} height={50} />
+            {owner}
+          </Link>
+          <div className="d-flex align-items-center">
+            <span>{updated_on}</span>
+            {is_owner && postPage && <DropdownMenu handleEdit={handleEdit} handleDelete={handleDelete} />}
+          </div>
+        </Media>
+      </Card.Body>
+      <Link to={`/posts/${id}`}>
+        <Card.Img src={image} alt={title} />
+      </Link>
+      <Card.Body>
+        {title && <Card.Title className="text-center">{title}</Card.Title>}
+        {description && <Card.Text>{description}</Card.Text>}
+        {country && <Card.Text>{country}</Card.Text>}
+        <hr className={styles.Line} />
+        <div className={styles.PostBar}>
+          {is_owner ? (
+            <OverlayTrigger
+              placement="top"
+              overlay={<Tooltip>You can't like your own post!</Tooltip>}
+            >
+              <i className="far fa-heart" />
+            </OverlayTrigger>
+          ) : like_id ? (
+            <span onClick={handleUnlike}>
+              <i className="fas fa-heart" />
+            </span>
+          ) : currentUser ? (
+            <span onClick={handleLike}>
+              <i className="far fa-heart" />
+            </span>
+          ) : (
+            <OverlayTrigger
+              placement="top"
+              overlay={<Tooltip>Log in to like posts!</Tooltip>}
+            >
+              <i className="far fa-heart" />
+            </OverlayTrigger>
+          )}
 
-                    {likes_number}
-                    <Link to={`/posts/${id}`}>
-                        <i className="far fa-comments" />
-                    </Link>
-                    {comments_number}
-                </div>
-            </Card.Body>
-        </Card>
-    );
+          {likes_number}
+          <Link to={`/posts/${id}`}>
+            <i className="far fa-comments" />
+          </Link>
+          {comments_number}
+        </div>
+        {showSuccessAlert && (
+          <Alert variant="success" onClose={() => setShowSuccessAlert(false)} dismissible>
+            Post deleted successfully!
+          </Alert>
+        )}
+        {showErrorAlert && (
+          <Alert variant="danger" onClose={() => setShowErrorAlert(false)} dismissible>
+            Failed to delete post. Please try again.
+          </Alert>
+        )}
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default Post;
