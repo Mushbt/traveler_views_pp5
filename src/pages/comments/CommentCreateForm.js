8a9243e00a94e208c0531fe641780375f97b399a
Mushbt <mustafa_habet@hotmail.com>
2023-12-12 08:08:21 +0000

    Add successful and unsuccessful alerts to commentEditForm and refactor commentCreateForm alerts to match

--- src/pages/comments/CommentCreateForm.js
+++ src/pages/comments/CommentCreateForm.js
@@ -1,31 +1,19 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, Form, InputGroup } from "react-bootstrap";
+import { Button, Form, InputGroup, Alert } from "react-bootstrap";
 import styles from "../../styles/CommentCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import Avatar from "../../components/Avatar";
 import { axiosRes } from "../../api/axiosDefaults";
-import AlertMessage from '../../components/AlertMessage';
 
 function CommentCreateForm(props) {
   const { post, setPost, setComments, profileImage, profile_id } = props;
   const [content, setContent] = useState("");
-  const [showAlert, setShowAlert] = useState(false);
-  const [alertVariant, setAlertVariant] = useState('success');
-  const [alertMessage, setAlertMessage] = useState('');
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
 
   const handleChange = (e) => {
     setContent(e.target.value);
-  };
-
-  const showAlertMessage = (variant, message) => {
-    setAlertVariant(variant);
-    setAlertMessage(message);
-    setShowAlert(true);
-
-    setTimeout(() => {
-      setShowAlert(false);
-    }, 5000);
   };
 
   const handleSubmit = async (e) => {
@@ -48,22 +36,30 @@ function CommentCreateForm(props) {
         ],
       }));
       setContent("");
-      showAlertMessage('success', 'Comment added successfully!');
+      setShowSuccessMessage(true);
+
+      setTimeout(() => {
+        setShowSuccessMessage(false);
+      }, 5000);
     } catch (err) {
       console.log(err);
-      showAlertMessage('danger', 'Failed to add comment. Please try again.');
+      setShowErrorMessage(true);
+
+      setTimeout(() => {
+        setShowErrorMessage(false);
+      }, 5000);
     }
   };
 
+  const successMessage = "Comment added successfully!";
+  const errorMessage = "Failed to add comment. Please try again.";
+
   return (
     <Form className="mt-2 text-center" onSubmit={handleSubmit}>
-      {showAlert && (
-        <AlertMessage
-          variant={alertVariant}
-          message={alertMessage}
-          onClose={() => setShowAlert(false)}
-        />
+      {showSuccessMessage && (
+        <Alert variant="success">{successMessage}</Alert>
       )}
+      {showErrorMessage && <Alert variant="danger">{errorMessage}</Alert>}
 
       <Form.Group>
         <InputGroup>
--- src/pages/comments/CommentEditForm.js
+++ src/pages/comments/CommentEditForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, Alert } from "react-bootstrap";
 import { axiosRes } from "../../api/axiosDefaults";
 import styles from "../../styles/CommentCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
@@ -7,6 +7,8 @@ import appStyles from "../../App.module.css";
 function CommentEditForm(props) {
     const { id, content, setShowEditForm, setComments } = props;
     const [formContent, setFormContent] = useState(content);
+    const [successMessage, setSuccessMessage] = useState(null);
+    const [errorMessage, setErrorMessage] = useState(null);
 
     const handleChange = (e) => {
         setFormContent(e.target.value);
@@ -30,13 +32,26 @@ function CommentEditForm(props) {
                         : comment;
                 }),
             }));
-            setShowEditForm(false);
+            setSuccessMessage("Comment updated successfully!");
+            setErrorMessage(null);
+            setTimeout(() => {
+                setSuccessMessage(null);
+                setShowEditForm(false);
+            }, 5000);
         } catch (err) {
+            console.log(err);
+            setErrorMessage("Failed to update comment. Please try again.");
+            setSuccessMessage(null);
+            setTimeout(() => {
+                setErrorMessage(null);
+            }, 5000);
         }
     };
 
     return (
         <Form className="mt-2 text-center" onSubmit={handleSubmit}>
+            {successMessage && <Alert variant="success">{successMessage}</Alert>}
+            {errorMessage && <Alert variant="danger">{errorMessage}</Alert>}
             <Form.Group className="pr-1">
                 <Form.Control
                     className={styles.Form}
@@ -47,15 +62,16 @@ function CommentEditForm(props) {
                 />
             </Form.Group>
             <div className="text-right">
-
-                <Button className={appStyles.button}
+                <Button
+                    className={appStyles.button}
                     disabled={!content.trim()}
                     onMouseDown={(e) => e.preventDefault()}
                     type="submit"
                 >
                     Update
                 </Button>
-                <Button className={appStyles.button}
+                <Button
+                    className={appStyles.button}
                     onClick={() => setShowEditForm(false)}
                     onMouseDown={(e) => e.preventDefault()}
                     type="button"
