ca0900dfa65d18f18f44fc0a8d88ad329c052fcd
Mushbt <mustafa_habet@hotmail.com>
2023-12-12 10:55:46 +0000

    Add Alert for when a profile is updated

--- src/pages/profiles/ProfileEditForm.js
+++ src/pages/profiles/ProfileEditForm.js
@@ -6,113 +6,140 @@ import { useCurrentUser, useSetCurrentUser } from "../../contexts/CurrentUserCon
 import appStyles from "../../App.module.css";
 
 const ProfileEditForm = () => {
-    const currentUser = useCurrentUser();
-    const setCurrentUser = useSetCurrentUser();
-    const { id } = useParams();
-    const history = useHistory();
-    const imageFile = useRef();
-  
-    const [profileData, setProfileData] = useState({
-      name: "",
-      description: "",
-      image: "",
-    });
-    const { name, description, image } = profileData;
-  
-    const [errors, setErrors] = useState({});
-  
-    useEffect(() => {
-      const handleMount = async () => {
-        if (currentUser?.profile_id?.toString() === id) {
-          try {
-            const { data } = await axiosReq.get(`/profiles/${id}/`);
-            const { name, description, image } = data;
-            setProfileData({ name, description, image });
-          } catch (err) {
-            console.log(err);
-            history.push("/");
-          }
-        } else {
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
+  const { id } = useParams();
+  const history = useHistory();
+  const imageFile = useRef();
+
+  const [profileData, setProfileData] = useState({
+    name: "",
+    description: "",
+    image: "",
+  });
+  const { name, description, image } = profileData;
+
+  const [errors, setErrors] = useState({});
+  const [showSuccessAlert, setShowSuccessAlert] = useState(false);
+  const [showErrorAlert, setShowErrorAlert] = useState(false);
+
+  useEffect(() => {
+    const handleMount = async () => {
+      if (currentUser?.profile_id?.toString() === id) {
+        try {
+          const { data } = await axiosReq.get(`/profiles/${id}/`);
+          const { name, description, image } = data;
+          setProfileData({ name, description, image });
+        } catch (err) {
+          console.log(err);
           history.push("/");
         }
-      };
-  
-      handleMount();
-    }, [currentUser, history, id]);
-  
-    const handleChange = (event) => {
-      setProfileData({
-        ...profileData,
-        [event.target.name]: event.target.value,
-      });
-    };
-  
-    const handleSubmit = async (event) => {
-      event.preventDefault();
-      const formData = new FormData();
-      formData.append("name", name);
-      formData.append("description", description);
-  
-      if (imageFile?.current?.files[0]) {
-        formData.append("image", imageFile?.current?.files[0]);
-      }
-  
-      try {
-        const { data } = await axiosReq.put(`/profiles/${id}/`, formData);
-        setCurrentUser((currentUser) => ({
-          ...currentUser,
-          profile_image: data.image,
-        }));
-        history.goBack();
-      } catch (err) {
-        console.log(err);
-        setErrors(err.response?.data);
+      } else {
+        history.push("/");
       }
     };
-  
-    const textFields = (
-      <>
-        <Form.Group>
-          <Form.Label>Profile description</Form.Label>
-          <Form.Control
-            as="textarea"
-            value={description}
-            onChange={handleChange}
-            name="description"
-            rows={7}
-            className={appStyles.Input}
-          />
-        </Form.Group>
-  
-        {errors?.description?.map((message, idx) => (
-          <Alert variant="warning" key={idx}>
-            {message}
-          </Alert>
-        ))}
-  
-        <Button
-          className={`my-3 ${appStyles.button}`}
-          onMouseDown={(e) => e.preventDefault()}
-          type="submit"
-        >
-          Save
-        </Button>
-  
-        <Button
-          className={`mx-3 ${appStyles.button}`}
-          onMouseDown={(e) => e.preventDefault()}
-          onClick={() => history.goBack()}
-        >
-          Cancel
-        </Button>
-      </>
-    );
-  
-    return (
-      <Form onSubmit={handleSubmit}>
+
+    handleMount();
+  }, [currentUser, history, id]);
+
+  const handleChange = (event) => {
+    setProfileData({
+      ...profileData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("description", description);
+
+    if (imageFile?.current?.files[0]) {
+      formData.append("image", imageFile?.current?.files[0]);
+    }
+
+    try {
+      const { data } = await axiosReq.put(`/profiles/${id}/`, formData);
+      setCurrentUser((currentUser) => ({
+        ...currentUser,
+        profile_image: data.image,
+      }));
+      setShowSuccessAlert(true);
+
+      setTimeout(() => {
+        history.goBack(); 
+      }, 5000);
+    } catch (err) {
+      console.error(err);
+      setErrors(err.response?.data);
+      setShowErrorAlert(true);
+    }
+  };
+
+  const textFields = (
+    <>
+      <Form.Group>
+        <Form.Label>Profile description</Form.Label>
+        <Form.Control
+          as="textarea"
+          value={description}
+          onChange={handleChange}
+          name="description"
+          rows={7}
+          className={appStyles.Input}
+        />
+      </Form.Group>
+
+      {errors?.description?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+
+      <Button
+        className={`my-3 ${appStyles.button}`}
+        onMouseDown={(e) => e.preventDefault()}
+        type="submit"
+      >
+        Save
+      </Button>
+
+      <Button
+        className={`mx-3 ${appStyles.button}`}
+        onMouseDown={(e) => e.preventDefault()}
+        onClick={() => history.goBack()}
+      >
+        Cancel
+      </Button>
+    </>
+  );
+
+  return (
+    <Form onSubmit={handleSubmit}>
       <Row>
         <Col className="d-none d-md-block p-0 p-md-2 text-center" md={8} lg={8}>
-          <Container className={appStyles.Content}>{textFields}</Container>
+          <Container className={appStyles.Content}>
+            {showSuccessAlert && (
+              <Alert
+                variant="success"
+                onClose={() => setShowSuccessAlert(false)}
+                dismissible
+              >
+                Profile updated successfully!
+              </Alert>
+            )}
+            {showErrorAlert && (
+              <Alert
+                variant="danger"
+                onClose={() => setShowErrorAlert(false)}
+                dismissible
+              >
+                Failed to update profile. Please try again.
+              </Alert>
+            )}
+            {textFields}
+          </Container>
         </Col>
         <Col className="py-2 p-0 p-md-2 text-center" md={4} lg={4}>
           <Container className={appStyles.Content}>
@@ -122,11 +149,12 @@ const ProfileEditForm = () => {
                   <Image src={image} fluid />
                 </figure>
               )}
-  
+
               <div>
                 <Form.Label
                   className={`${appStyles.button}`}
-                  htmlFor="image-upload">
+                  htmlFor="image-upload"
+                >
                   Change your picture
                 </Form.Label>
               </div>
@@ -146,16 +174,16 @@ const ProfileEditForm = () => {
               />
             </Form.Group>
             {errors?.image?.map((message, idx) => (
-                <Alert variant="warning" key={idx}>
-                  {message}
-                </Alert>
-              ))}
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
             <div className="d-md-none">{textFields}</div>
           </Container>
         </Col>
       </Row>
     </Form>
-    );
-  };
-  
-  export default ProfileEditForm;
+  );
+};
+
+export default ProfileEditForm;
