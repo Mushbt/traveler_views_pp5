03476bf0aa00e4fa27ae8e8a5d21d8dc13c447ec
Mushbt <mustafa_habet@hotmail.com>
2023-12-13 07:13:32 +0000

    Add comments to axiosDefaults file

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
+// Unique URL for deployed API project on Heroku 
 axios.defaults.baseURL = "https://traveler-view-90e3792ff7f0.herokuapp.com/"
+// Expected data format by API
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data"
+// Avoid any CORS errors when sending cookies
 axios.defaults.withCredentials = true;
 
+// Refreshing access tokens
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
